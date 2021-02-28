456b7f86b3813ae11d77a2625d4d666a77ea1d0e
KoichiHirai <i.remember_tt@icloud.com>
2021-02-28 19:02:17 +0900

    vercel deploy

--- api/index.js
+++ api/index.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT||3000;
+
+app.get("/", function(req,res){
+	res.send("Hello World");
+});
+
+/*
+app.listen(3000, function(){
+});
+*/
+
+(process.env.NOW_REGION) ? module.exports = app.listen(PORT);
