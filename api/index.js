f9a9b647c6cb5dfb6f260e8eb6bf7436ef13e6b1
KoichiHirai <i.remember_tt@icloud.com>
2021-03-27 20:06:33 +0900

    3/27 task completed

--- api/index.js
+++ api/index.js
@@ -2,17 +2,26 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT||3000;
 
-app.get("/", function(req,res){
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+app.get("/", function(req,res) {
 	res.send("Hello World");
+	// res.json({'msg': "Hello World"})
 });
 
-app.get("/webhook", function(req,res){
+app.get("/webhook", (req,res) => {
 	res.send("Hello Webhook");
 });
 
-/*
+app.post("/api", (req,res) => {
+	console.log(req.body);
+	res.send(req.body);
+});
+
+
 app.listen(3000, function(){
 });
-*/
 
-(process.env.NOW_REGION) ? module.exports = app : app.listen(PORT);
+
+//(process.env.NOW_REGION) ? module.exports = app : app.listen(PORT);
